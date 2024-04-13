e66e51d9a5099ed2317bed7c3b2c5afa0c9afe42
rahulsingh <rahulsingh@i.am.rahul>
2024-04-13 13:38:25 +0530

    arithemetic-operators

---
+++ 03/script.js
@@ -0,0 +1,39 @@
+// arithemetic operators - operands (values, variables, etc.)
+//                         operators (+, -, *, /)
+
+let students = 30;
+
+// students = students + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students / 2;
+// students = students ** 2;
+// let extraStudents = students % 4;
+
+// console.log(students);
+// console.log(extraStudents);
+
+// AUGMENTED ASSIGNMENT OPERATORS!
+
+// students += 1;
+// students -= 1;
+// students *= 3;
+// students /= 3;
+// students **= 3;
+// students %= 3;
+
+// students++; // INCREMENT.
+// students--; // DECREMENT.
+
+// console.log(students);
+
+/**
+ * Operators precedence!
+ * 1. parenthesis ()
+ * 2. exponents
+ * 3. multiplication & division & modulus
+ * 4. addition & substraction
+ */
+
+let result = 1 + 2 * 3 + 4 ** 2;
+console.log(result);
