04f5f6907bbd30ed3f48973751792f17004bb554
rahulsingh <rahulsingh@i.am.rahul>
2024-04-13 12:46:57 +0530

    intro-to-js

---
+++ 01/script.js
@@ -0,0 +1,18 @@
+// console.log(`Hello`);
+// console.log("I like pizza!");
+
+// window.alert(`Hello`);
+// window.alert(`I like pizza!`);
+
+// This is a single line comment.
+
+/**
+ * This
+ * is
+ * a
+ * multiline
+ * comment.
+ */
+
+document.getElementById("main-text").textContent = `Hello`;
+document.getElementById("description").textContent = `I like pizza`;
