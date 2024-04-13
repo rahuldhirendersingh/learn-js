2a100b5fc24d27c6a55b817edf28a274fe4140e3
rahulsingh <rahulsingh@i.am.rahul>
2024-04-13 14:10:08 +0530

    type conversion

---
+++ 05/script.js
@@ -0,0 +1,26 @@
+// Type Conversion - Change the datatype of a value to another type
+//      (strings, numbers, booleans)
+
+// let age = window.prompt("How old are you?");
+
+// age = Number(age);
+
+// age += 1;
+
+// console.log(age, typeof age);
+
+// let x = "butter chicken";
+// let y = "butter chicken";
+// let z = "butter chicken";
+
+let x = "";
+let y = "";
+let z = "";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
