e034e3fbec8d1f6c5d40c70edf9a6a34843786b1
rahulsingh <rahulsingh@i.am.rahul>
2024-04-13 15:41:10 +0530

    math object

---
+++ 08/script.js
@@ -0,0 +1,33 @@
+// Math - build in object that provide's a
+//        collection of properties and methods.
+
+// console.log(Math.PI);
+// console.log(Math.E);
+
+let a = 3.36;
+let b = 2;
+let c;
+
+// c = Math.round(a); // round-off
+// c = Math.ceil(a); // round-up
+// c = Math.floor(a); // round-down
+// c = Math.trunc(a); // removes decimal portion
+// c = Math.pow(b, 3);
+// c = Math.sqrt(49);
+// c = Math.log(10);
+// c = Math.sin(45);
+// c = Math.cos(45);
+// c = Math.tan(45);
+// c = Math.abs(-12);
+
+// console.log(c);
+
+x = 33;
+y = 89;
+z = 46;
+
+let max = Math.max(x, y, z);
+console.log(max);
+
+let min = Math.min(x, y, z);
+console.log(min);
